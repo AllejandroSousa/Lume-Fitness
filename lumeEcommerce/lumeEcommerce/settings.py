d8dd3dc2f187b1ec2395052228d31f2f70b6e532
Allejandro <allejandro.soousa@academico.ufpb.br>
2025-03-19 19:24:08 -0300

    Building the initial models

--- lumeEcommerce/lumeEcommerce/settings.py
+++ lumeEcommerce/lumeEcommerce/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'store', #Django app
 ]
 
 MIDDLEWARE = [
@@ -115,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_FILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'static/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ lumeEcommerce/store/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True)  #Only unique categories
+
+    class Meta:
+        verbose_name_plural = "categories"  #Renaming the Category page in Django's admin panel
+
+    def __str__(self):
+        return self.name  #Renaming categories on the Category page in Django's admin panel
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    brand = models.CharField(max_length=250, default='Sem marca')
+    description = models.TextField(blank=True)  #Optional
+    slug = models.SlugField(max_length=250)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        verbose_name_plural = "products"  #Renaming the Product page in Django's admin panel
+
+    def __str__(self):
+        return self.title  #Renaming products on the Product page in Django's admin panel
