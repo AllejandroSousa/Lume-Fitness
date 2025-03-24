2eef292dd2f0b0ef1c5ad59a279f7cf14e33a831
Allejandro <allejandro.empresarial@gmail.com>
2025-03-24 01:57:50 -0300

    Add authentication features
    Users can now log in, log out, and delete their accounts

--- lumeEcommerce/account/forms.py
+++ lumeEcommerce/account/forms.py
@@ -1,8 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms.widgets import PasswordInput, TextInput
 
 
+# Registration form
 class CreateUserForm(UserCreationForm):
 
     username = forms.CharField(
@@ -45,5 +47,30 @@ class CreateUserForm(UserCreationForm):
 
         if len(email) >= 350:
             raise forms.ValidationError('Email inválido: muito longo!')
-        
+
         return email
+
+
+# Login form
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput(), label='Usuário')
+    password = forms.CharField(widget=PasswordInput(), label='Senha')
+
+
+# Update form
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(
+        label="Usuário",
+        help_text="Obrigatório. Máximo de 150 caracteres. Apenas letras, números e @/./+/-/_."
+    )
+    email = forms.EmailField(label="E-mail")
+    password = None
+
+    class Meta:
+
+        model = User
+        fields = ['username', 'email']
+        exclude = ['password1', 'password2']
+    
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
--- lumeEcommerce/account/views.py
+++ lumeEcommerce/account/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import redirect, render
-from .forms import CreateUserForm
+from .forms import CreateUserForm, LoginForm, UpdateUserForm
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from .token import user_tokenizer_generate
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -51,7 +54,6 @@ def email_verification(request, uidb64, token):
         return redirect('email-verification-failed')
 
 
-
 def email_verification_sent(request):
     return render(request, 'account/registration/email-verification-sent.html')
 
@@ -62,3 +64,61 @@ def email_verification_success(request):
 
 def email_verification_failed(request):
     return render(request, 'account/registration/email-verification-failed.html')
+
+
+def my_login(request):
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+
+                return redirect('dashboard')
+
+    context = {'form': form}
+
+    return render(request, 'account/my-login.html', context=context)
+
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect('store')
+
+
+@login_required(login_url='my-login')
+def dashboard(request):
+    return render(request, 'account/dashboard.html')
+
+
+@login_required(login_url='my-login')
+def profile_management(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('dashboard')
+    
+    user_form = UpdateUserForm(instance=request.user)
+    context = {'user_form': user_form}
+
+    return render(request, 'account/profile-management.html', context=context)
+
+
+@login_required(login_url='my-login')
+def delete_account(request):
+    user = User.objects.get(id=request.user.id)
+
+    if request.method == 'POST':
+        user.delete()
+
+        return redirect('store')
+
+    return render(request, 'account/delete-account.html')
